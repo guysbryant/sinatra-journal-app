e117cd5270a18167e422883f8d2b30706d19603c
guysbryant <guy.s.bryant@gmail.com>
2019-05-07 18:15:47 -0400

    fix strinng typo

--- db/migrate/20190507215902_create_users_table.rb
+++ db/migrate/20190507215902_create_users_table.rb
@@ -2,7 +2,7 @@ class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.strinng :email
+      t.string :email
       t.string :password_digest #allows bcrypt to encrypt password
       t.timestamps null: false
     end
