73aa8b9e02050b20dfd37f353a29fc1e74f75a6b
guysbryant <guy.s.bryant@gmail.com>
2019-05-08 16:18:19 -0400

    add more detail in comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base 
-    has_secure_password #this comes from bcrypt, compares plain string to hashing algorithm
+    has_secure_password #this comes from bcrypt, compares plain string to hashing algorithm, gives authenticate method
     has_many :journal_entries
 end
