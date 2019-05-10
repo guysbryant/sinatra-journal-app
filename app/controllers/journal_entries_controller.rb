73cfdde63725305c671f1fafb05ba197057dd653
guysbryant <guy.s.bryant@gmail.com>
2019-05-10 17:01:36 -0400

    Add routes for new, show, and index

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,10 +1,19 @@
 class JournalEntries < ApplicationController
 
     #get journal_entries/new to render a form to create a new entry
+    get '/journal_entries/new' do 
+        erb :"/journal_entries/new"
+    end
 
     #post journal_entries to create a new journal entry
 
     #show route for a journal entry
+    get '/journal_entries/show' do 
+        erb :"/journal_entries/show"
+    end
 
     #index route for all journal entries
+    get '/journal_entries/index' do 
+        erb :"/journal_entries/index"
+    end
 end
