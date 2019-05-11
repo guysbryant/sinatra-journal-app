c3ad645351baecf65347dffabd6b1863f0431d69
guysbryant <guy.s.bryant@gmail.com>
2019-05-11 06:42:31 -0400

    Add ability to edit entry and update entry in ActiveRecord. Create set_journal_entry helper method

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -20,17 +20,34 @@ class JournalEntries < ApplicationController
 
     #show route for a journal entry
     get '/journal_entries/:id' do 
-        @journal_entry = JournalEntry.find(params[:id])
+        set_journal_entry
         erb :"/journal_entries/show"
     end
 
     #send us to journal_entries/edit.erb
     get '/journal_entries/:id/edit' do
+        set_journal_entry
         erb :"/journal_entries/edit"
+    end
+
+    #update journal entry in DB
+    patch '/journal_entries/:id' do
+        #1. Find journal entry
+        set_journal_entry
+        #2. Update the journal entry
+        @journal_entry.update(content: params[:content])
+        #3. redirect to journal entry show page
+        redirect "/journal_entries/#{@journal_entry.id}"
     end
 
     #index route for all journal entries
     get '/journal_entries/index' do 
         erb :"/journal_entries/index"
+    end
+
+    private 
+
+    def set_journal_entry
+        @journal_entry = JournalEntry.find(params[:id])
     end
 end
