8104203dc8da01282123af133383b4f3e933a63a
guysbryant <guy.s.bryant@gmail.com>
2019-05-13 14:53:38 -0400

    Add ActiveRecord presence validations for name and email

--- app/controllers/users_controler.rb
+++ app/controllers/users_controler.rb
@@ -30,11 +30,12 @@ class UsersController < ApplicationController
     end
 
     post '/users' do 
-        if params[:name] != "" && params[:email] != "" && params[:password] != ""
-            @user = User.create(params)
+        @user = User.new(params)
+        if @user.save
             session[:user_id] = @user.id
             redirect "/users/#{@user.id}"
         else
+            flash[:errors] = "Account creation failure: #{@user.errors.full_messages.to_sentence}"
             redirect "/signup"
         end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base 
     has_secure_password #this comes from bcrypt, compares plain string to hashing algorithm, gives authenticate method
+    validates :name, presence: true 
+    validates :email, presence: true 
+    validates :email, uniqueness: true 
+
     has_many :journal_entries
 end
