d4a249c5ec3c7f2429fabf3c1277c8f072f7df1d
guysbryant <guy.s.bryant@gmail.com>
2019-05-13 14:22:27 -0400

    Add flash messages

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -17,8 +17,10 @@ class JournalEntries < ApplicationController
         end
         if params[:content] != ""
             @journal_entry = JournalEntry.create(content: params[:content], user_id: current_user.id)
+            flash[:message] = "Congrats on your new journal entry."
             redirect "/journal_entries/#{@journal_entry.id}"
         else
+            flash[:message] = "Something went wrong."
             redirect :"/journal_entries/new"
         end
     end
